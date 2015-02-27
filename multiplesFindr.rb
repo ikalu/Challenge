fee384ad1c05090a19979d4db7c56480bbefe3c2
Ifeanyi <ikalu13@gmail.com>
2015-02-26 21:29:00 -0500

    Given any string; the algorithm will compress and uncompress it

---
+++ multiplesFindr.rb
@@ -0,0 +1,26 @@
+class Multiples
+
+  def find_multiples
+    sum = 0
+
+    (1..1000).each do |num|
+      if num % 15 == 0
+        print num
+        print ' '
+        sum += num
+        puts sum
+      elsif (num % 3 == 0 or num % 5 == 0)
+        print num
+        print ' '
+        sum += num
+        puts sum
+      end
+    end
+
+    puts sum
+  end
+
+end
+
+num = Multiples.new
+num.find_multiples
---
+++ stringCompressr.rb
@@ -0,0 +1,57 @@
+class StrCompression
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def manipulate_string
+    string = word.split('')
+    compr_str = compress(string)
+    puts compr_str
+    uncompr_str = uncompress(compr_str)
+    puts uncompr_str
+  end
+
+  def compress(string)
+    new_string = ''
+    counter = 0
+
+    string.each_index do |i|
+      str = new_string.chop
+      if !str.end_with?(string[i])
+        new_string << string[i] 
+
+        counter += 1
+      end
+
+      ((i + 1)..((string.length) - 1)).each do |a|
+        break if !new_string.end_with?(string[a])
+        counter += 1 if new_string.end_with?(string[a])
+      end
+
+      new_string << counter.to_s if counter != 0
+      counter = 0
+    end
+
+    new_string
+  end
+
+  def uncompress(compr_str)
+    str = compr_str.split('')
+
+    old_string = ''
+
+    str.each_index do |i|
+      next if i.odd?
+
+      old_string << str[i]*str[i + 1].to_i
+    end
+
+    old_string
+  end
+
+end
+
+str = StrCompression.new("aabcccccaaa")
+str.manipulate_string
